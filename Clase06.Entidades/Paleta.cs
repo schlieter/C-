1d659fa829c638f8b84144835a1b3d4a6b3fb956
schlieter <c.schlieter@hotmail.com>
2017-04-19 20:48:20 -0300

    Add files via upload

--- Clase06.Entidades/Paleta.cs
+++ Clase06.Entidades/Paleta.cs
@@ -11,11 +11,12 @@ namespace Clase06.Entidades
         private Tempera[] _colores;
         private int _cantidadMaximaColores;
 
-        
-        
 
-        private Paleta():this(5)
-        {}
+
+
+        private Paleta()
+            : this(5)
+        { }
         private Paleta(int a)
         {
             this._colores = new Tempera[a];
@@ -43,7 +44,7 @@ namespace Clase06.Entidades
             return p1.Mostrar();
         }
 
-        public static Boolean operator ==(Paleta p1, Tempera t1)//VER SI ESTA BIEN
+        public static Boolean operator ==(Paleta p1, Tempera t1)
         {
             foreach (Tempera t2 in p1._colores)
             {
@@ -62,81 +63,70 @@ namespace Clase06.Entidades
 
         public static Paleta operator +(Paleta p1, Tempera t1)
         {
-            int i, j = 0;
-            for (i = 0; i < p1._colores.GetLength(0); i++)
+            int i, j;
+            i = p1.ObtenerIndice(t1);
+            j = p1.ObtenerIndice();
+            if (i < 0)
             {
-                if (p1._colores[i] == t1)
-                {
-                    p1._colores[i] = p1._colores[i] + t1;
-                    j = 1;
-                    break;
-                }
+                if (j > -1)
+                    p1._colores[j] = t1;
             }
-            if (j == 0)
+            else
             {
-                for (i = 0; i < p1._colores.GetLength(0); i++)
-                {
-                    if (p1._colores[i] == null)
-                    {
-                        p1._colores.SetValue(t1, i);
-                        break;
-                    }
-                }
-
+                p1._colores[i] = p1._colores[i] + t1;
             }
             return p1;
         }
-         
+
         public static Paleta operator -(Paleta p1, Tempera t1)
         {
             int i;
-            for (i = 0; i < p1._colores.GetLength(0); i++)
+            i = p1.ObtenerIndice(t1);
+
+            if (i > -1)
             {
-                if (p1._colores[i] == t1)
+                p1._colores[i] = p1._colores[i] - t1;
+                if (p1._colores[i] < 1)
                 {
-                    p1._colores[i] = p1._colores[i] - t1;
-                    if (p1._colores[i] == 0)////COMO SE QUE COMPARA EL 0 CON LA CANTIDAD
-                        p1._colores[i] = null;
-                    break;
-                } 
+                    p1._colores[i] = null;
+                }
+
             }
             return p1;
         }
         public static Paleta operator +(Paleta p1, Paleta p2)
         {
-            int a,i,j,flag;
+            int a, i;
             a = p1._colores.GetLength(0) + p2._colores.GetLength(0);
             Paleta p3 = new Paleta(a);
-            for(i=0;i<p1._colores.GetLength(0);i++)
+            p1._colores.CopyTo(p3._colores, 0);
+            for (i = 0; i < p2._colores.GetLength(0); i++)
             {
-                flag=0;
-                for(j=0; j<p2._colores.GetLength(0);j++)
-                {
-                    if(p1._colores[i] == p2._colores[j])
-                    {
-                        p3._colores[i]= p1._colores[i] + p2._colores[j];//hay que hacer un add o sumando ya lo agrega a la paleta??
-                        flag=1;
-                        break;
-                    }
-                }
-                //flag esta en 0 add
-                if(flag == 0)
-                {
-                    p3._colores[i] = (Tempera)p1._colores.GetValue(i);//ESTA BIEN???
-                    p3._colores.SetValue(p1._colores.GetValue(i),i);//CUAL DE LAS DOS
-                }
+                p3 = p3 + p2._colores[i];
             }
-            for(i=0;i<p2._colores.GetLength(0);i++){
-            //for nuevo para agregar lo de la segunda paleta que falta
-            //si paleta2 no coincide con ninguno de paleta3 add, si coincide no hace nada porque ya lo agrego antes
-            }
+            return p3;
         }
+
 
         public int ObtenerIndice()
         {
-            
-    }
- 
-
+            int i;
+            for (i = 0; i < this._colores.GetLength(0); i++)
+            {
+                if (this._colores[i] == null)
+                    return i;
+            }
+            return -1;
+        }
+        public int ObtenerIndice(Tempera t1)
+        {
+            int i;
+            for (i = 0; i < this._colores.GetLength(0); i++)
+            {
+                if (this._colores[i] == t1)
+                    return i;
+            }
+            return -1;
+        }
     }
 }
